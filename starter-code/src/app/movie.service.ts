c69f9f53e7149949261b5ded33b83699c5011d4b
Alberto Salmerón Tapia <alberto@MacBook-Pro-de-Alberto.local>
2018-02-21 16:43:24 +0100

    Iteration 1

---
+++ starter-code/src/app/movie.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+
+export interface MoviesInterface {
+  id: Number;
+  title: String;
+  poster: String;
+  synopsis: String;
+  genres: Array<String>;
+  year: Number;
+  director: String;
+  actors: Array<String>;
+  hours: Array<String>;
+  room: Number;
+}
+
+@Injectable()
+export class MovieService {
+
+  movies: Array<MoviesInterface>;
+
+  constructor() { }
+
+  getMovies(){
+    return this.movies;
+  }
+
+  getMovie(id){
+    for(let i = 0; i < this.movies.length; i++){
+      if(this.movies[i].id == id){
+        return this.movies;
+      }
+    }
+  }
+
+}
+
+
